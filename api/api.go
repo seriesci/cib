45a050df185bd06f41798ee9af6be6e8a82ba5ab
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-11-12 21:41:04 +0100

    make repo work for github, travis and circle

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -43,9 +44,33 @@ func sha() (string, error) {
 	return strings.TrimSpace(string(out)), nil
 }
 
+func repo() (string, error) {
+	// github actions
+	repo, ok := os.LookupEnv("GITHUB_REPOSITORY")
+	if ok {
+		return repo, nil
+	}
+
+	// travis ci
+	repo, ok = os.LookupEnv("TRAVIS_REPO_SLUG")
+	if ok {
+		return repo, nil
+	}
+
+	// circle ci
+	if _, ok = os.LookupEnv("CIRCLECI"); ok {
+		username := os.Getenv("CIRCLE_PROJECT_USERNAME")
+		reponame := os.Getenv("CIRCLE_PROJECT_REPONAME")
+		return username + "/" + reponame, nil
+	}
+
+	return "", errors.New("cannot find repo in environment variables")
+}
+
 // Post something.
 // currently only works with GitHub Actions.
 func Post(value, series string) (*http.Response, error) {
+	// get commit hash
 	s, err := sha()
 	if err != nil {
 		return nil, err
@@ -56,18 +81,19 @@ func Post(value, series string) (*http.Response, error) {
 		"sha":   {s},
 	}
 
-	// IMPORTANT: this only works for GitHub Actions at the moment
-	// seriesci/cib
-	repo := os.Getenv("GITHUB_REPOSITORY")
+	// get repo in form owner/repo
+	r, err := repo()
+	if err != nil {
+		return nil, err
+	}
 
-	u := fmt.Sprintf("https://seriesci.com/api/repos/%s/%s/combined", repo, series)
+	u := fmt.Sprintf("https://seriesci.com/api/repos/%s/%s/combined", r, series)
 	req, err := http.NewRequest(http.MethodPost, u, strings.NewReader(data.Encode()))
 	if err != nil {
 		return nil, err
 	}
 
-	token := os.Getenv("SERIESCI_TOKEN")
-	req.Header.Set("Authorization", fmt.Sprintf("Token %s", token))
+	req.Header.Set("Authorization", fmt.Sprintf("Token %s", os.Getenv("SERIESCI_TOKEN")))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	return http.DefaultClient.Do(req)
